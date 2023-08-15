d5a2e5e53a39688ca76aa1c6c250c6827bd97c94
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-08-16 00:30:09 +0100

    added copy contructor/copy assignement op

--- ex02/Array.cpp
+++ ex02/Array.cpp
@@ -1,14 +1,45 @@
 #include "Array.h"
 
-
-Array::Array()
+template<typename T>
+Array<T>::Array()
 {
-    T[10];
-}
-Array::Array(unsigned int n)
-{
+    T[];
 }
 
-Array::~Array()
+template<typename T>
+Array<T>::Array(unsigned int n)
+{
+    arr = new T[n];
+}
+
+template<typename T>
+Array<T>::Array(const Array& other) {
+    if(other.getSize() > 0)
+    {
+        this->arr = new T[other.getSize()];
+        for (unsigned int i = 0; i < this->getSize(); i++)
+        {
+            this->arr[i] = other.arr[i];
+        }
+    }
+}
+
+template<typename T>
+const Array<T>& Array<T>::operator=(const Array& other) {
+    if(this != &other)
+    {
+        this->_n = other.getSize();
+    }
+    return (*this);
+}
+
+template<typename T>
+unsigned int Array<T>::getSize() const{
+    return this->_n;
+}
+
+
+template<typename T>
+Array<T>::~Array()
 {
 }
--- ex02/Array.h
+++ ex02/Array.h
@@ -7,13 +7,15 @@ template<typename T>
 class Array
 {
     private:
-        /* data */
+        unsigned int _n;
+        T *arr;
     public:
         Array();
         Array(unsigned int n);
+        Array(const Array& other);
+        unsigned int getSize(void) const;
+        const Array& operator=(const Array& other);
         ~Array();
 };
-
-
 
 #endif
