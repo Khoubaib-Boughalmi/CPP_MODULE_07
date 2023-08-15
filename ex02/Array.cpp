c33ae062516a0a9eeddb66cd2d8d075051bc65db
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-08-15 23:59:03 +0100

    started ex02

---
+++ ex02/Array.cpp
@@ -0,0 +1,14 @@
+#include "Array.h"
+
+
+Array::Array()
+{
+    T[10];
+}
+Array::Array(unsigned int n)
+{
+}
+
+Array::~Array()
+{
+}
---
+++ ex02/Array.h
@@ -0,0 +1,19 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <iostream>
+
+template<typename T>
+class Array
+{
+    private:
+        /* data */
+    public:
+        Array();
+        Array(unsigned int n);
+        ~Array();
+};
+
+
+
+#endif
