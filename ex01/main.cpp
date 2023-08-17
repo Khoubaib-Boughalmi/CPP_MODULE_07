92f3c50096ed8075885e61a5a6aa9aa19ff9f6bc
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-08-17 10:10:01 +0100

    removed hidden directory

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,13 +1,9 @@
 #include "iter.h"
 
-
 int main( void ) {
     double arr[5] = {1.5, 2.5, 3.5, 4.5, 5};
     iter(arr, 5, square<double>);    
     for (size_t i = 0; i < 5; i++)
-    {
         std::cout << arr[i] << std::endl;
-    }
-
     return 0;
 }
